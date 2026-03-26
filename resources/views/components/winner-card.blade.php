725e1d3438167d4cd1af1897ebcd2233d5211422
Bumpyy <fsghassan2429d@gmail.com>
2026-03-26 11:02:52 +0700

    add address

--- resources/views/components/winner-card.blade.php
+++ resources/views/components/winner-card.blade.php
@@ -32,6 +32,20 @@
                 <span class="truncate" title="{{ $winner->submission->user->email }}">
                     {{ $winner->submission->user->maskedEmail }}</span>
             </div>
+
+            <div class="flex items-center">
+                <svg class="mr-2 h-4 w-4 text-gray-400" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                    stroke-linejoin="round">
+                    <path
+                        d="M15 22a1 1 0 0 1-1-1v-4a1 1 0 0 1 .445-.832l3-2a1 1 0 0 1 1.11 0l3 2A1 1 0 0 1 22 17v4a1 1 0 0 1-1 1z" />
+                    <path d="M18 10a8 8 0 0 0-16 0c0 4.993 5.539 10.193 7.399 11.799a1 1 0 0 0 .601.2" />
+                    <path d="M18 22v-3" />
+                    <circle cx="10" cy="10" r="3" />
+                </svg>
+                <span class="truncate" title="{{ $winner->submission->user->address }}">
+                    {{ $winner->submission->user->address }}</span>
+            </div>
         </div>
     </div>
 </div>
