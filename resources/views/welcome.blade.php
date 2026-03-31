9c9f50ae025d5f69053d695322b97393cbd16707
Bumpyy <fsghassan2429d@gmail.com>
2026-03-31 13:34:14 +0700

    update plane

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,15 +11,15 @@
             class="max-w-800 min-w-200 mx-auto my-[10vh] grid w-fit grid-cols-2 flex-wrap items-start justify-center gap-10">
 
             <article
-                class="max-w-150 flex h-full w-full justify-center rounded-2xl bg-white p-2.5 text-center shadow-[0_30px_30px_-25px_rgba(65,51,183,0.25)]">
+                class="max-w-150 flex h-full w-full flex-col items-center justify-center rounded-2xl bg-white p-2.5 text-center shadow-[0_30px_30px_-25px_rgba(65,51,183,0.25)]">
                 <a class="inline-flex items-center justify-between gap-2 rounded-md bg-white"
                     href="{{ route('raffle.show', [WinnerPrizeEnum::TRIP->value]) }}">
-                    <img class="w-full" src="{{ asset('img/plane-prize.png') }}" alt="">
+                    <img src="{{ asset('img/plane-prize-2.png') }}" alt="">
                 </a>
             </article>
 
             <article
-                class="max-w-150 w-full rounded-2xl bg-white p-2.5 text-center shadow-[0_30px_30px_-25px_rgba(65,51,183,0.25)]">
+                class="max-w-150 flex h-full w-full flex-col items-center justify-center rounded-2xl bg-white p-2.5 text-center shadow-[0_30px_30px_-25px_rgba(65,51,183,0.25)]">
                 <a class="inline-flex items-center justify-between gap-2 rounded-md bg-white px-14 py-4"
                     href="{{ route('raffle.show', [WinnerPrizeEnum::MONEY->value]) }}">
                     <img src="{{ asset('img/money-prize.png') }}" alt="">
